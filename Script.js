632b51b411a63616b3ac10374b1bcb96e53f93d5
JorgeVera04 <159754400+JorgeVera04@users.noreply.github.com>
2024-03-20 23:10:02 -0600

    Update Script.js
    Implementación de restricción de mayúsculas

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-//Encriptación
+// Encriptación
 function encriptarTexto() {
     var textoOriginal = document.getElementById('textoOriginal').value;
 
@@ -10,11 +10,11 @@ function encriptarTexto() {
 
     document.getElementById('textoEncriptado').value = textoEncriptado;
 
-    //Limpiar el área de texto del texto encriptado
+    // Limpiar el área de texto del texto original
     document.getElementById('textoOriginal').value = "";
 }
 
-//Desencriptaer
+// Desencriptar
 function desencriptarTexto() {
     var textoEncriptado = document.getElementById('textoEncriptado').value;
 
@@ -26,52 +26,63 @@ function desencriptarTexto() {
 
     document.getElementById('textoOriginal').value = textoDesencriptado;
 
-    //Limpiar el área de texto del texto desencriptado
+    // Limpiar el área de texto del texto encriptado
     document.getElementById('textoEncriptado').value = "";
 }
 
-//Copiar texto encriptado al portapapeles
+// Copiar texto 
 function copiarTexto() {
     var textoEncriptado = document.getElementById('textoEncriptado');
 
     textoEncriptado.select();
-    textoEncriptado.setSelectionRange(0, 99999); //Para móviles
+    textoEncriptado.setSelectionRange(0, 99999); // Para móviles
     document.execCommand("copy");
 }
 
-//Mostrar mensaje de error
+// Mostrar mensaje de error
 function mostrarMensajeError(mensaje) {
     var mensajeError = document.getElementById('mensajeError');
     mensajeError.textContent = mensaje;
     mensajeError.classList.add('mostrar'); // Agregar la clase para mostrar el mensaje
 }
 
-//Ocultar mensaje de validacion
+// Ocultar mensaje de error
 function ocultarMensajeError() {
     var mensajeError = document.getElementById('mensajeError');
     mensajeError.classList.remove('mostrar');
 }
 
-//Valida encriptación
+// Validar encriptación
 function validarEncriptacion() {
     var textoOriginal = document.getElementById('textoOriginal').value;
 
     if (textoOriginal.trim() === "") {
         mostrarMensajeError("El texto está vacío, no es posible encriptarlo.");
+    } else if (/[A-Z]/.test(textoOriginal)) { // Validar mayúsculas
+        mostrarMensajeError("No se pueden agregar mayúsculas al texto para encriptar.");
+        limpiarCajasTexto();
     } else {
         ocultarMensajeError();
         encriptarTexto();
     }
 }
 
-//Valida desencriptación
+// Validar desencriptación
 function validarDesencriptacion() {
     var textoEncriptado = document.getElementById('textoEncriptado').value;
 
     if (textoEncriptado.trim() === "") {
         mostrarMensajeError("El texto está vacío, no es posible desencriptarlo.");
+    } else if (/[A-Z]/.test(textoEncriptado)) {
+        mostrarMensajeError("No se pueden agregar mayúsculas al texto para desencriptarlo.");
+        limpiarCajasTexto();
     } else {
         ocultarMensajeError();
         desencriptarTexto();
     }
+}
+
+function limpiarCajasTexto() {
+    document.getElementById('textoOriginal').value = "";
+    document.getElementById('textoEncriptado').value = "";
 }
